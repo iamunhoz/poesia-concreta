aa367f33799c8581731ae1981dba7b09bb2b2693
Ivã Munhoz <iamunhoz@gmail.com>
2022-09-11 19:54:46 -0300

    jumping char

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [palavra, setPalavra] = useState('magicka')
 
+  const [floatCharIdx, setFloatCharIdx] = useState(1)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setFloatCharIdx(prev => prev < palavra.length ? prev + 1 : 0)
+    }, 500)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [floatCharIdx])
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='p-3 w-100 h-100 d-flex flex-column align-items-center justify-content-evenly'>
+      <div className='mb-3 d-flex w-100 justify-content-center'>
+        {palavra.split('').map((char, idx) => {
+          return (
+            <div
+              key={idx}
+              className='char'
+              style={floatCharIdx === idx ? {position: 'relative', transform: 'translateY(-10px)'} : undefined}
+            >
+              {char}
+            </div>
+          )
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input style={{width: 200}} value={palavra} onChange={e => setPalavra(e.currentTarget.value)} />
     </div>
   )
 }
